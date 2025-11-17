d13f5a517181332300a2828c20cac4a30bae70cc
amelialexandra <amelialexandra.it@gmail.com>
2025-11-17 15:01:52 +0700

    Initial Commit

---
+++ TryLottie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryLottie
+//
+//  Created by Amelia Alexandra on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryLottie/TryLottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryLottieApp.swift
+//  TryLottie
+//
+//  Created by Amelia Alexandra on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TryLottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
