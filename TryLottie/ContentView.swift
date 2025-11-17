4b3c0a6ded6b2d4c1c42db687fd893ccf6601f15
amelialexandra <amelialexandra.it@gmail.com>
2025-11-17 19:16:04 +0700

    add running cat

--- TryLottie/ContentView.swift
+++ TryLottie/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
     var body: some View {
@@ -14,6 +15,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            LottieView(animation: .named("Running Cat"))
+                .playing(loopMode: .loop)
         }
         .padding()
     }
